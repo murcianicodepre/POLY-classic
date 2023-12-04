686732ae788edfe18b24cf4255aee38f81dc209e
murcianicodepre <diegoso14@hotmail.com>
2023-12-04 15:03:44 +0100

    Inital file upload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # POLY-classic
+Simple 90s inspired CPU raytracer
+
+* 1024×768 4:3 rendering output
+* .poly scripts for describing scenes
+* .ply 3D models
+* .png 1024×1024 textures
+* Single or multithreaded CPU rendering
---
+++ include/poly-classic.h
@@ -0,0 +1,28 @@
+#ifndef _POLY_CLASSIC_
+#define _POLY_CLASSIC_
+
+/*
+    POLY classic ~ main header
+    Diego Párraga Nicolás ~ diegojose.parragan@um.es
+*/
+
+#include "yaml.h"
+#include "yaml-cpp/exceptions.h"
+
+// Render pipeline defs
+#define WIDTH 1024
+#define HEIGHT 768
+#define DEFAULT_NUM_THREADS 1
+#define TEXTURE_SIZE 1024
+#define FOV 60.0f
+#define AR 1.333333333f
+
+// Math defs
+#define PI 3.14159265f
+#define ALPHA 0.017453292f
+#define EPSILON 1e-4f
+
+// Incomplete classes 
+
+
+#endif
